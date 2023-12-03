471c5f7ab35414990bf9bf67e4a19e182b3ceff5
hmshoberg <145800386+hmshoberg@users.noreply.github.com>
2023-12-03 14:38:54 -0600

    Updated images and schema

--- EmployeeSQL/Table Schema.sql
+++ EmployeeSQL/Table Schema.sql
@@ -1,46 +1,36 @@
-CREATE TABLE departments(
-	Dept_No VARCHAR (5)Primary Key,
-	Dept_Name VARCHAR (30),
-	CONSTRAINT fk_dept_manager FOREIGN KEY (Dept_No) REFERENCES dept_manager(Dept_No)
+CREATE TABLE titles(
+	title_id VARCHAR(5) PRIMARY KEY,
+	title VARCHAR(30)
 );
 
-CREATE TABLE dept_emp (
-	Employee_No VARCHAR (10) Primary Key,
-	Dept_No VARCHAR (5),
-	CONSTRAINT fk_dept_emp FOREIGN KEY (Dept_No) REFERENCES departments(Dept_No)
+CREATE TABLE employees(
+	emp_no VARCHAR(10) PRIMARY KEY,
+	emp_title_id VARCHAR(5) REFERENCES titles(title_id),
+	birth_date DATE,
+	first_name VARCHAR(30),
+	last_name VARCHAR(45),
+	sex VARCHAR,
+	hire_date DATE
+);
+
+CREATE TABLE departments (
+    dept_no VARCHAR(5) PRIMARY KEY,
+    dept_manager_emp_no VARCHAR(10) REFERENCES employees(emp_no),
+    dept_name VARCHAR(30)
 );
 
 CREATE TABLE dept_manager(
-	Dept_No VARCHAR (5) Primary Key,
-	Employee_No VARCHAR (10),
-	CONSTRAINT fk_employees FOREIGN KEY (Employee_No) REFERENCES employees(Employee_No)
+	dept_no VARCHAR(5) REFERENCES departments(dept_no),
+    emp_no VARCHAR(10) PRIMARY KEY REFERENCES employees(emp_no)
 );
 
-ALTER TABLE employees
-ADD CONSTRAINT unique_employee_no UNIQUE (Employee_No);
-
-CREATE TABLE employees (
-    Employee_No VARCHAR (10),
-    Emp_Title_ID VARCHAR (5),
-    Birth_Date DATE,
-    First_name VARCHAR (50),
-    Last_name VARCHAR (50),
-    Sex VARCHAR,
-    Hire_Date DATE,
-    PRIMARY KEY (Employee_No, Emp_Title_ID),
-    CONSTRAINT fk_salaries FOREIGN KEY (Employee_No) REFERENCES Salaries(Employee_No)
+CREATE TABLE dept_emp(
+	emp_no VARCHAR(10) REFERENCES employees(emp_no),
+    dept_no VARCHAR(5),
+    PRIMARY KEY (emp_no, dept_no)
 );
 
-CREATE TABLE salaries (
-	Employee_No VARCHAR (10) Primary Key,
-	Salary INTEGER 
-);
-
-ALTER TABLE employees
-ADD CONSTRAINT unique_emp_title_id UNIQUE (Emp_Title_ID);
-
-CREATE TABLE titles(
-	ID INTEGER Primary Key,
-	Emp_Title_ID VARCHAR (5),
-	CONSTRAINT fk_employees FOREIGN KEY (Emp_Title_ID) REFERENCES employees(Emp_Title_ID)
+CREATE TABLE salaries(
+	emp_no VARCHAR(10) REFERENCES employees(emp_no),
+    salary INTEGER
 );
